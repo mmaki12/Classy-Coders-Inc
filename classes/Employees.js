c534a64f90d74213f499089e70e89acbff416012
mmaki12 <mmaki12@hotmail.com>
2023-10-25 12:58:34 +0100

    new

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,9 +1,34 @@
 class Employees {
-
-
-
-    
-}
+    #salary
+    #isHired
+    constructor(name, position, salary) {
+      this.name = name;
+      this.position = position;
+      this.#salary = salary; 
+      this.#isHired = true; 
+    }
+  
+    getSalary() {
+      return this.#salary;
+    }
+  
+    setSalary(amount) {
+      this.#salary = amount;
+    }
+  
+    getStatus() {
+      return this.#\isHired;
+    }
+  
+    setStatus(command) {
+      if (command === "hire") {
+        this.#isHired = true;
+      } else if (command === "fire") {
+        this.#isHired = false;
+      }
+    }
+  }
+  
 
 module.exports = {
     Employees,
